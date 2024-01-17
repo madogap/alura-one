ec193e9639432a841ae2346b3edb3e1ba223e0dd
madogap <madogap@gmail.com>
2024-01-17 15:51:16 -0300

    First commit

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,70 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Jogo do número secreto";
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+};
+
+function exibirMensagemInicial() {
+  exibirTextoNaTela("h1", "Jogo do número secreto");
+  exibirTextoNaTela("p", "Escolha um número de 1 a 10");  
+};
+exibirMensagemInicial();
+
+function verificarChute() {
+  let chute = document.querySelector("input").value;
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou");
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+    exibirTextoNaTela("p", mensagemTentativas);
+    document.getElementById('reiniciar').removeAttribute('disabled');
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "O número secreto é menor");
+    } else {
+      exibirTextoNaTela("p", "O número é maior");
+    }
+    tentativas++;
+    limparCampo();
+  }
+  console.log(chute == numeroSecreto);
+};
+
+//Gerar números aleatórios sem parametros com retorno
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+};
+
+function limparCampo() {
+  chute = document.querySelector('input');
+  chute.value= '';
+};
+
+function reiniciarJogo(){
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo();
+  tentativas = 1;
+  exibirMensagemInicial();
+  document.getElementById('reiniciar').setAttribute('disabled', true);
+};
+
+
+
+/*
+Tipo de Função|	Exemplo de Código	| Uso
+Sem retorno e sem parâmetro	| function saudacao() { ... } |	Execução de bloco de código simples.
+Sem retorno e com parâmetro	| function cumprimentar(nome) { ... } |	Execução de bloco de código com argumentos.
+Com retorno e sem parâmetro	| function gerarNumeroAleatorio() { ... }	| Cálculo e retorno de um valor específico.
+Com retorno e com parâmetro	| function somar(a, b) { ... }	| Cálculo e retorno baseado em argumentos.
+Função anônima	| let saudacao = function() { ... };	| Definição de função sem nome localmente.
+Arrow function	| let quadrado = x => x * x;	| Definição concisa de funções curtas.
+
+
+*/
